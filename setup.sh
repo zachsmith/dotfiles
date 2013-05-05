c7520fefbf403f86cfc5e732f51bab3d44ea4e00
Zach Smith <zach@substantial.com>
2013-05-04 18:45:30 -0700

    add setup script - very basic/rough/blunt

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+## shell
+ln -s ~/dotfiles/bash ~/.bash
+ln -s ~/.bash/bash_profile ~/.bash_profile
+ln -s ~/.bash/bashrc ~/.bashrc
+
+## git
+ln -s ~/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/dotfiles/gitignore ~/.gitignore
+
+## ruby
+ln -s ~/dotfiles/irbrc ~/.irbrc
+ln -s ~/dotfiles/pryrc ~/.pryrc
+ln -s ~/dotfiles/pryrc_rails ~/.pryrc_rails
+
+## tmux
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+
+
+
