07a25f2ff35885deff97f2d1deaf332d545d70fc
Zach Smith <zach@substantial.com>
2013-05-04 21:57:27 -0700

    add git and search

--- setup.sh
+++ setup.sh
@@ -5,6 +5,10 @@ ln -s ~/dotfiles/bash ~/.bash
 ln -s ~/.bash/bash_profile ~/.bash_profile
 ln -s ~/.bash/bashrc ~/.bashrc
 
+## search
+ln -s ~/dotfiles/ackrc ~/.ackrc
+ln -s ~/dotfiles/agignore ~/.agignore
+
 ## git
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/gitignore ~/.gitignore
