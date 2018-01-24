934040ec4d8127db9126cff91f805f9b2b6d4467
Zach Smith <zach@crosswise.xyz>
2018-01-23 21:40:43 -0800

    make changes to work on WSL

--- setup.sh
+++ setup.sh
@@ -5,21 +5,6 @@ ln -s ~/dotfiles/bash ~/.bash
 ln -s ~/.bash/bash_profile ~/.bash_profile
 ln -s ~/.bash/bashrc ~/.bashrc
 
-## search
-ln -s ~/dotfiles/ackrc ~/.ackrc
-ln -s ~/dotfiles/agignore ~/.agignore
-
 ## git
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/gitignore ~/.gitignore
-
-## ruby
-ln -s ~/dotfiles/irbrc ~/.irbrc
-ln -s ~/dotfiles/pryrc ~/.pryrc
-ln -s ~/dotfiles/pryrc_rails ~/.pryrc_rails
-
-## tmux
-ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
-
-
-
